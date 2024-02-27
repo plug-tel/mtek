bcaafb5edd2bc80e83ed5f9535b5f705bc4ebdeb
GmachMariem <mariemgmech@outlook.fr>
2024-02-27 18:17:35 +0000

    first commit

---
+++ server-app/src/main/java/com/m/tek/TekApplication.java
@@ -0,0 +1,13 @@
+package com.m.tek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekApplication.class, args);
+	}
+
+}
---
+++ server-app/src/test/java/com/m/tek/TekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m.tek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
